f3ed678b5d2144e4f754d549b5c9128f14810dd2
jesusorejarena <jesusorejarena@gmail.com>
2023-10-26 03:44:36 -0500

    fix

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,11 +4,11 @@ import { NavItem } from '../../types/components';
 export default function Navbar() {
 	const Item = ({ href, text }: NavItem) => {
 		return (
-			<a href={href}>
+			<Link to={href}>
 				<p className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
 					{text}
 				</p>
-			</a>
+			</Link>
 		);
 	};
 
